7469122a5b63bc9d0b11bc908e55c569109169d9
Timothy Clem <timothy.clem@gmail.com>
2019-07-30 08:57:29 -0700

    Add some very basic tests

---
+++ test/Spec.hs
@@ -0,0 +1,22 @@
+module Main where
+
+import Test.Hspec
+import Data.Languages
+
+main :: IO ()
+main = hspec $ do
+  describe "detect language for path" $ do
+    it "can detect Ruby by file extension" $
+      languageName <$> languageForPath "test.rb" `shouldBe` Just "Ruby"
+
+    it "can detect Ruby by filename" $
+      languageName <$> languageForPath "Rakefile" `shouldBe` Just "Ruby"
+
+    it "returns Nothing for unknown files" $
+      languageName <$> languageForPath "noideawhatthisis" `shouldBe` Nothing
+
+  describe "languages" $
+    it "parsed languages.yml" $ do
+      length languages `shouldBe` 519
+      length languagesByExtension `shouldBe` 1117
+      length languagesByFileName `shouldBe` 235
